14a185797719146c27880f1e074109af4cc4f0a0
nitishgupta978 <nitishgupta9785@gmail.com>
2024-10-17 12:46:07 +0530

    Add UpdateHibernate class for update the data of db

---
+++ src/main/java/com/transparentcoding/hibernate/hibernate_first_project/UpdateHibernateData.java
@@ -0,0 +1,28 @@
+package com.transparentcoding.hibernate.hibernate_first_project;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.transparentcoding.hibernate.hibernate_first_project.dto.HibernateDemo;
+
+public class UpdateHibernateData {
+	public static void main(String[] args) {
+
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		configuration.addAnnotatedClass(HibernateDemo.class);
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+		HibernateDemo demo = session.get(HibernateDemo.class, 1);
+		demo.setFirstname("NITISH "); // only update first name and remain same with change object id
+
+		session.beginTransaction();
+		session.update(demo);
+		session.getTransaction().commit();
+		System.out.println(demo);
+
+	}
+}
